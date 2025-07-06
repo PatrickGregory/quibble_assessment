cdc1d80939e28e203139ed2970e38412351b59d2
Kantee <patrickgregoryekene@gmail.com>
2025-07-06 19:05:38 +0100

    completed the clone

---
+++ script.js
@@ -0,0 +1,24 @@
+// DOM Elements
+// const body = document.querySelector('body');
+const content = document.getElementById('content');
+const modalBtn = document.getElementById('modalBtn');
+// const modal = document.getElementById('modal');
+const modalBg = document.getElementsByClassName('display-none');
+const closeModalBtn = document.getElementById('close-modal');
+
+
+// Display modal on modalBtn click
+// Add modal-bg to the body
+// Put display none on content div
+// Display modal
+
+// Event Listeners
+modalBtn.addEventListener('click', openModal);
+
+
+// Funtions
+
+function openModal() {
+
+  // modalBg.classList.add('modal-bg')
+}
